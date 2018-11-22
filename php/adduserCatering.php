9bb99edb44092442e7e5a8439b1d47cd635f7555
sandunsameera <sandunsameera25@gmail.com>
2018-11-22 18:08:46 +0530

    innitioal commit

--- php/adduserCatering.php
+++ php/adduserCatering.php
@@ -1,26 +1,27 @@
 <?php
-include "connection.php";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "aaa";
 
-if(isset($_POST['submit'])){
-    addUser();
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+if (isset($_POST['submit'])){}
+
+$id = $_POST["id"];
+$username = $_POST["cName"];
+$age = $_POST["cAge"];
+$exp = $_POST["cExp"];
+
+$sql = "INSERT INTO caters (id, cName, cAge,cExp)
+VALUES ('$id', '$username', '$age','$exp')";
 }
 
-function addUser(){
-
-    $id = e($_POST['id']);
-    $username = e($_POST['c-name']);
-    $age = e($_POST['c-age']);
-    $exp = e($_POST['c-exp']);
-
-$sql = "INSERT INTO caters (id,c-name, c-age, c-exp)
-VALUES  ('$id','$username','$age','$exp')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
-
-}
 $conn->close();
+
 ?>
