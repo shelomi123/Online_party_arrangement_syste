0f9ecfb6ccb721fa9caa924e65f071e887d2ad6c
sandunsameera <sandunsameera25@gmail.com>
2018-11-27 19:10:57 +0530

    user delete

--- php/userdelPhotography.php
+++ php/userdelPhotography.php
@@ -1,41 +1,18 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "aaa";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
+require_once "connection.php";
 
 if (isset($_POST['submit'])){
-
-$id = $_POST('p-id');
-
-// sql to delete a record
-$sql = mysqli_query($dbname, "DELETE FROM photographers WHERE p-id=$id");
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-} else {
-    echo "Error deleting record: " . $conn->error;
+$id = $_POST["id"];
+$query = "DELETE FROM photographers WHERE id=$id;";
+mysqli_query($conn,$query);
+mysqli_close($conn);
 }
-}
-
-$conn->close();
 ?>
-
 <html>
 <title>Admin</title>
 
 <head>
-    <link rel="stylesheet" href="../css/admin.css">
     <link rel="stylesheet" href="../css/adduser.css">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 
 <body>
@@ -50,14 +27,14 @@ $conn->close();
     <h3>Deleting User</h3>
 
     <div class="container">
-        <form action="userdelPhotography.php">
+        <form action="userdelPhotography.php" method="post">
           <label for="name">NAME</label>
           <input type="text" id="fname" name="name" placeholder="Enter name..">
       
           <label for="id">USER_ID</label>
           <input type="text" id="id" name="id" placeholder="Enter id..">
         
-          <input type="submit" value="Submit">
+          <input type="submit" value="Submit" name="submit">
 
         </form>
     </div>
